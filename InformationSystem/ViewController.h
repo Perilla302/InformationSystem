c45a3889feaca995c99284344252e9b145b7f592
Hongjin Su <HongjinSu@hongjins-macbook-air.local>
2015-10-09 15:05:56 -0400

    Initial Commit

---
+++ InformationSystem/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InformationSystem
+//
+//  Created by Hongjin Su on 10/9/15.
+//  Copyright © 2015 Hongjin Su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
